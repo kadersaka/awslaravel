9b27f5046082e3f820741cef42177ae058ded0dc
kadersaka <kadersaka@gmail.com>
2021-04-30 10:01:13 +0100

    first commit

---
+++ app/Http/Controllers/TrustedLogoController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrustedLogo;
+use Illuminate\Http\Request;
+
+class TrustedLogoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+               //
+        $path = $request->file('logo')->store('logos', 's3');
+      //  $path = $request->file('logo')->store('logos');
+        return $path;
+       // dd($request->file('logo'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TrustedLogo  $trustedLogo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrustedLogo $trustedLogo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TrustedLogo  $trustedLogo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrustedLogo $trustedLogo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TrustedLogo  $trustedLogo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrustedLogo $trustedLogo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TrustedLogo  $trustedLogo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrustedLogo $trustedLogo)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_30_080926_create_trusted_logos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrustedLogosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trusted_logos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trusted_logos');
+    }
+}
